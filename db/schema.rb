3eede982d1ba33d7687d284fde0c0228e8a800d0
ljones1877 <jlorin7@gmail.com>
2019-10-19 18:48:05 +0000

    Lesson 29

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_17_185133) do
+ActiveRecord::Schema.define(version: 2019_10_19_172322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2019_10_17_185133) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.float "latitude"
+    t.float "longtitude"
     t.index ["user_id"], name: "index_places_on_user_id"
   end
 
