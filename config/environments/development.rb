f16d6714a9fa3654f6879768ab058dfc3025ea3d
ljones1877 <jlorin7@gmail.com>
2019-10-30 02:30:43 +0000

    Lesson 41

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,18 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: 'localhost:3030' }
+  config.action_mailer.default_url_options = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV['jlorin7@gmail.com'],
+    password: ENV['Xbox1877!']
+  }
+
+  config.action_mailer.default_url_options = {host: 'localhost:3030'}
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
