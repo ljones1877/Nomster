5d5e77301256f521b648d1201f0d6688062663d1
ljones1877 <jlorin7@gmail.com>
2019-10-30 01:58:57 +0000

    Lesson 39

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,6 +6,7 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:              'AWS',                        
     aws_access_key_id:     ENV["AKIAUPBZPVHI3Y5K2P4M"],        
-    aws_secret_access_key: ENV["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"],       
-  config.fog_directory  = ENV["lorin-jones-nomster"]              
+    aws_secret_access_key: ENV["8aeaS9XbR6Fxd+iwW7M2nEVagVF3xbzWf14P3z0z"],      
+  }
+  config.fog_directory  = ENV["lorin-jones-nomster"]            
 end
