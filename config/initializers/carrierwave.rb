01fec018dba8e46227663719c7ce80d5256008d0
ljones1877 <jlorin7@gmail.com>
2019-10-31 01:59:20 +0000

    Lesson 44 fixed

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,12 @@
 # config/initializers/carrierwave.rb
-
 CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        
   config.fog_public = false
   config.fog_credentials = {
     provider:              'AWS',                        
-    aws_access_key_id:     ENV["AKIAUPBZPVHI3Y5K2P4M"],        
-    aws_secret_access_key: ENV["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"],      
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        
+    aws_secret_access_key: ENV["AWS_SECRET_KEY"],
+    aws_region: ENV["AWS_REGION"]      
   }
-  config.fog_directory  = ENV["lorin-jones-nomster"]            
+  config.fog_directory  = ENV["AWS_BUCKET"]            
 end
