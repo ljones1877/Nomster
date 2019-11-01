bda6cfde93384f0fb99bc8a41ddc03ac8db60da0
ljones1877 <jlorin7@gmail.com>
2019-11-01 02:15:26 +0000

    Lesson 47

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'places#index'
   resources :places do
-    resources :comments, only: :create  
-    resources :users, :only => :show
-      end
-  resources :places do
+    resources :comments, only: :create
     resources :photos, only: :create
-    resources :users, :only => :show
   end
+  resources :users, only: :show
 end
